dc18310940c2a4d3b0a7c3eacb8c840be9077617
slavo <slavo2@users.noreply.github.com>
2015-05-20 13:14:01 +0200

    Private method Klarna::fixMessageEncoding() renamed to _fixMessageEncoding() and updated: support for intl, mbstring and iconv php extensions fallback to ASCII using regular expression possibility to specify input and output encoding as method arguments

--- Klarna.php
+++ Klarna.php
@@ -3666,7 +3666,7 @@ class Klarna
             $status = $xmlrpcresp->faultCode();
 
             if ($status !== 0) {
-                $errorMessage = $this->fixMessageEncoding($xmlrpcresp->faultString());
+                $errorMessage = $this->_fixMessageEncoding($xmlrpcresp->faultString());
                 throw new KlarnaException($errorMessage, $status);
             }
 
@@ -4421,15 +4421,27 @@ class Klarna
     /**
      * Fixes originalMessage encoding
      *
-     * @param string $originalMessage
+     * @param $originalMessage
+     * @param string $inputEncoding (defaults to ISO-8859-1)
+     * @param string $outputEncoding (defaults to UTF-8)
      * @return string
      */
-    private function fixMessageEncoding($originalMessage)
+    private function _fixMessageEncoding($originalMessage, $inputEncoding = 'ISO-8859-1', $outputEncoding = 'UTF-8')
     {
-        $possibleInputEncodings = ['ISO-8859-1', 'UTF-8'];
-        $outputEncoding = mb_internal_encoding();
-
-        return mb_convert_encoding($originalMessage, $outputEncoding, $possibleInputEncodings);
+        if (extension_loaded('intl') and class_exists('UConverter')) {
+            $fixedMessage = UConverter::transcode($originalMessage, $outputEncoding, $inputEncoding);
+            return $fixedMessage;
+        }
+        if (extension_loaded('mbstring')) {
+            $fixedMessage = mb_convert_encoding($originalMessage, $outputEncoding, $inputEncoding);
+            return $fixedMessage;
+        }
+        if (extension_loaded('iconv')) {
+            $fixedMessage = iconv($inputEncoding, $outputEncoding, $originalMessage);
+            return $fixedMessage;
+        }
+        $fixedMessage = preg_replace('#[[:^ascii:]]#', '?', $originalMessage);
+        return $fixedMessage;
     }
 } //End Klarna
 
